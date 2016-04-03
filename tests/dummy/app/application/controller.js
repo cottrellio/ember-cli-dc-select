e750113540ef155907d4873215cb27ee42b2ebb3
Dave Cottrell <dave@eventboard.io>
2016-04-03 12:45:15 -0600

    Added more test options to dummy app

--- tests/dummy/app/application/controller.js
+++ tests/dummy/app/application/controller.js
@@ -11,7 +11,7 @@ export default Ember.Controller.extend({
     {
       label: 'Test 1',
       value: 1,
-      disabled: true
+      disabled: false
     },
     {
       label: 'Test 2',
@@ -21,6 +21,16 @@ export default Ember.Controller.extend({
     {
       label: 'Test 3',
       value: 3,
+      disabled: true
+    },
+    {
+      label: 'Test 4',
+      value: 4,
+      disabled: false
+    },
+    {
+      label: 'Test 5',
+      value: 5,
       disabled: false
     }
   ]),
